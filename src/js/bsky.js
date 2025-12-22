71a56be8a7c67baf629a4ccf4e389cf23fe83e06
Tasory Tasory <andr3393399@gmail.com>
2025-12-22 14:59:44 +0400

    initial commit

---
+++ src/js/bsky.js
@@ -0,0 +1,14 @@
+
+// VIBECODE SHIT WARNING!!!!!
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=tasory.is-a.dev&limit=1');
+        const data = await response.json();
+        const postText = data.feed[0].post.record.text;
+        document.getElementById('bskyText').textContent = postText;
+    } catch (error) {
+        console.error('Ошибка:', error);
+        document.getElementById('bskyText').textContent = 'Не удалось загрузить последний пост';
+    }
+});
---
+++ src/js/lastfm.js
@@ -0,0 +1,36 @@
+
+// VIBECODE SHIT WARNING!!!!!
+
+// vars
+const parts = ['4c67', '2513', '1b21', '3c8d', '0955', '896b', '938a', '34b0']; // dont steal my a p i key plsss
+const a_p_i_klyuchik = parts.join('');
+const username = 'tasoryy';
+let currentTrackName = '';
+
+// script
+function fetchCurrentTrack() {
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${a_p_i_klyuchik}&format=json`)
+        .then(response => response.json())
+        .then(data => {
+            const recentTracks = data.recenttracks.track;
+            if (recentTracks.length > 0) {
+                const currentTrack = recentTracks[0];
+                const newTrackName = currentTrack.name;
+
+                if (newTrackName !== currentTrackName) {
+                    currentTrackName = newTrackName;
+                    document.getElementById('album-title').innerText = currentTrack.album['#text'];
+                    document.getElementById('track-title').innerText = currentTrack.name;
+                    document.getElementById('artist-name').innerText = currentTrack.artist['#text'];
+                    document.getElementById('album-cover').src = currentTrack.image[3]['#text'];
+                }
+            } else {
+                document.getElementById('album-title').innerText = 'No data';
+                document.getElementById('track-title').innerText = '';
+            }
+        })
+        .catch(error => console.error('error:', error));
+}
+
+setInterval(fetchCurrentTrack, 1000);
+fetchCurrentTrack();
